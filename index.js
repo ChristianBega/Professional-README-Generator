0a99bb7356ea6722276263e1372bc633c7ee39d0
Chris B <cbega1227@gmail.com>
2022-11-19 18:21:57 -0800

    added psuedo text file.

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
-// TODO: Include packages needed for this application
+// import the modules from utils
+const inquirer = require("inquirer");
+const fs = require("fs");
+console.log(inquirer);
+console.log(fs);
 
 // TODO: Create an array of questions for user input
+// spread operator ...
 const questions = [];
+// Please provide a table of contents
+
+// Please provide a title for your project :
+// Please provide a description :
+// Please provide any installation instructions :
+// Please provide any usage information :
+// Please provide any credits/contributions :
+// Please choose a license :
+// Please choose a badge :
+// Please provide any features :
+// How can others contribute ?
+// Where can others reach out to ask questions (email,git,etc links)
+
+
+
 
 // TODO: Create a function to write README file
+// look at mini project week7 for help
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
